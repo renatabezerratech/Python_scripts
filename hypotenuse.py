e77b35873e222e151627f07758c05b56ae4ed139
RB <renatabezerratecnologia@gmail.com>
2024-08-26 19:22:04 -0300

    obs interpretador

--- hypotenuse.py
+++ hypotenuse.py
@@ -1,11 +1,11 @@
-catetoOposto = float(input("Digite o cateto oposto: "))
-catetoAdjacente = float(input("Digite o cateto adjacente: "))
-hipotenusa = (catetoOposto ** 2 + catetoAdjacente ** 2) ** (1/2)
+cateto_oposto = float(input("Digite o cateto oposto: "))
+cateto_adjacente = float(input("Digite o cateto adjacente: "))
+hipotenusa = (cateto_oposto ** 2 + cateto_adjacente ** 2) ** (1/2)
 print("A hipotenusa é: {:.2f}".format(hipotenusa))
 
 
 # from math import hypot   <---
-# catetoOposto = float(input("Digite o cateto oposto: "))
-# catetoAdjacente = float(input("Digite o cateto adjacente: "))
-# hipotenusa = hypot(catetoOposto, catetoAdjacente)   <---
+# cateto_oposto = float(input("Digite o cateto oposto: "))
+# cateto_adjacente = float(input("Digite o cateto adjacente: "))
+# hipotenusa = hypot(cateto_oposto, cateto_adjacente)   <---
 # print("A hipotenusa é: {:.2f}".format(hipotenusa))
